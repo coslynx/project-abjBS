d55b522d9d50440ac8ff9b8fa64a6255b46e3c4f
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-06-07 16:16:48 +0530

    Added: `discord-moderation-bot/src/commands/moderationCommands.js`

---
+++ discord-moderation-bot/src/commands/moderationCommands.js
@@ -0,0 +1,14 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.once('ready', () => {
+    console.log('Bot is ready');
+});
+
+client.on('messageCreate', async message => {
+    if (message.content === '!ping') {
+        message.reply('Pong!');
+    }
+});
+
+client.login('your-token-goes-here');
